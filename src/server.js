259845a78278b3d78f1e7214603020d399b58bdd
Luiz Rioja <lulyrioja@gmail.com>
2021-12-31 22:27:20 -0300

    :alien: Fixing project structure

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+
+//Iniciando a aplicação
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Iniciando a conexão do BD
+mongoose.connect(
+  "mongodb+srv://API:NodeAPIPassword@clusterapi.lqg8u.gcp.mongodb.net/nodeapi?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+//Importa models
+requireDir("./models");
+
+//Importação das rotas
+app.use("/api", require("./routes"));
+
+//Definindo a porta padrão
+app.listen(3001);
