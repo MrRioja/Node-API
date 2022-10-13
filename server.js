d506ee48967d6075d72b63750b64a06dbb9a4507
Luiz Rioja <lulyrioja@gmail.com>
2022-10-13 14:55:18 -0300

    chore: change mongodb connection string

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const requireDir = require('require-dir');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 //Iniciando a aplicação
 const app = express();
@@ -9,13 +9,17 @@ app.use(express.json());
 app.use(cors());
 
 //Iniciando a conexão do BD
-mongoose.connect('mongodb+srv://API:NodeAPIPassword@clusterapi.lqg8u.gcp.mongodb.net/nodeapi?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb+srv://API:NodeAPIPassword@clusterapi.lqg8u.gcp.mongodb.net/nodeapi?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/node-api", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Importa models
-requireDir('./src/models');
+requireDir("./src/models");
 
 //Importação das rotas
-app.use('/api', require('./src/routes'));
+app.use("/api", require("./src/routes"));
 
 //Definindo a porta padrão
 app.listen(3001);
